27ce9982d1b868bcc4b859d649b54761ffe613b2
lily <102743831+lilylilylily123@users.noreply.github.com>
2023-02-03 10:42:03 +0100

    animations!!!!!!

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 func main() {
-	fs := http.FileServer(http.Dir("./public/start/"))
-	fs2 := http.FileServer(http.Dir("./public/start/projects/"))
-	http.Handle("/start/", http.StripPrefix("/start/", fs))
+	fs := http.FileServer(http.Dir("./public/main/"))
+	fs2 := http.FileServer(http.Dir("./public/main/projects/"))
+	http.Handle("/main/", http.StripPrefix("/main/", fs))
 	http.Handle("/projects/", http.StripPrefix("/projects/", fs2))
 	http.ListenAndServe(":6969", nil)
 }
